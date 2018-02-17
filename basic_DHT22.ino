b6980942dd1524ea79e2f73bac90bd75d1d9d1ee
Javier Barrera <jtbarrera@gmail.com>
2018-02-17 17:57:36 -0600

    switched to new DHT library

--- basic_DHT22.ino
+++ basic_DHT22.ino
@@ -1,91 +1,33 @@
-/*
-Code for NodeMCU:
-Tempature
-humidity
+#include "DHTesp.h"
 
-output:
-print to serial line
+DHTesp dht;
 
-Original Code and reference material
-https://github.com/bruhautomation/ESP-MQTT-JSON-Multisensor
-https://www.losant.com/blog/getting-started-with-the-esp8266-and-dht22-sensor
-*/
-
-#include <DHT.h>
-
-/**************************** PIN DEFINITIONS *******************************************/
-#define DHTPIN    4
-#define DHTTYPE   DHT22
-
-/**************************** SENSOR DEFINITIONS ****************************************/
-int calibrationTime = 0;
-DHT dht(DHTPIN, DHTTYPE);
-
-/********************************** START SETUP*****************************************/
-void setup() {
-
+void setup()
+{
   Serial.begin(115200);
-  Serial.setTimeout(2000);
-  
-  // Wait for serial to initialize.
-  while(!Serial) { }
-  
-  Serial.println("Device Started");
-  Serial.println("-------------------------------------");
-  Serial.print("calibrating sensor ");
-  for (int i = 0; i < calibrationTime; i++) {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println("Running DHT!");
-  Serial.println("-------------------------------------");
+  Serial.println();
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
 
-  Serial.begin(115200);
-  delay(10);
+  dht.setup(4); // data pin 2
 }
 
-/********************************** MAIN LOOP*******************************************/
-int timeSinceLastRead = 0;
-void loop() {
+void loop()
+{
+  delay(dht.getMinimumSamplingPeriod()+8000);
 
-  // Report every 2 seconds.
-  if(timeSinceLastRead > 2000) {
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
-    float t = dht.readTemperature();
-    // Read temperature as Fahrenheit (isFahrenheit = true)
-    float f = dht.readTemperature(true);
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
 
-    // Check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t) || isnan(f)) {
-      Serial.println("Failed to read from DHT sensor!");
-      timeSinceLastRead = 0;
-      return;
-    }
-
-    // Compute heat index in Fahrenheit (the default)
-    float hif = dht.computeHeatIndex(f, h);
-    // Compute heat index in Celsius (isFahreheit = false)
-    float hic = dht.computeHeatIndex(t, h, false);
-
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.print(" *C ");
-    Serial.print(f);
-    Serial.print(" *F\t");
-    Serial.print("Heat index: ");
-    Serial.print(hic);
-    Serial.print(" *C ");
-    Serial.print(hif);
-    Serial.println(" *F");
-
-    timeSinceLastRead = 0;
-  }
-  delay(100);
-  timeSinceLastRead += 100;
+  Serial.print(dht.getStatusString());
+  Serial.print("\t");
+  Serial.print(humidity, 1);
+  Serial.print("\t\t");
+  Serial.print(temperature, 1);
+  Serial.print("\t\t");
+  Serial.print(dht.toFahrenheit(temperature), 1);
+  Serial.print("\t\t");
+  Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
+  Serial.print("\t\t");
+  Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
 }
+
